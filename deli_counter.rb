19a420db89aea8469090eb4590d4317e219de48f
Jason Park <jjpark987@gmail.com>
2023-04-10 16:43:45 -0700

    commit tree a612c4b95e93b91969533398dae5170ceb844c1d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+require "pry"
+
+katz_deli = [joe, bob, sue]
+
+def line(customers)
+    if customers.length == 0
+        puts "The line is currently empty."
+    else
+        message = "The line is currently: "
+        customers.each_with_index do |customer, index| 
+            message << "#{index + 1}. #{customer} "
+        end
+        puts message
+    end
+end
+binding.pry
